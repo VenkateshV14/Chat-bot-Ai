6d53225f5192882047b40b5c347b4783d8250ea2
VenkateshV14 <vakamudilavenkatesh@gmail.com>
2025-04-15 18:12:12 +0530

    updated

--- terraform/modules/vpc/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "project_name" {}
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-variable "public_subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
-}
